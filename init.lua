03ee07aae6ccfffe2ac7e550e97046d097f7bc89
tyler-grimes <tyler.d.grimes@gmail.com>
2025-03-15 19:00:51 -0600

    plugin changes

--- init.lua
+++ init.lua
@@ -985,6 +985,10 @@ require('lazy').setup({
       }
     end,
   },
+
+  {
+    'tpope/vim-surround',
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -998,7 +1002,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
